0164eb13bffbe1d32c6db893394cf9a3224653e4
kuan <apkapb@gmail.com>
2022-05-23 19:49:35 +0800

    feat: update favicon api & add dps

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,3 +1,3 @@
-// export const FAVICON_API_URL = 'https://api2.misiai.com/favicon/v1?size=260&url=';
+export const FAVICON_API_URL = 'https://api2.misiai.com/favicon/v1?size=260&url=';
 //https://favicon.cccyun.cc
-export const FAVICON_API_URL = 'https://favicon.cccyun.cc/';
+// export const FAVICON_API_URL = 'https://favicon.cccyun.cc/';
